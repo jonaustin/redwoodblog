8a5049e44461edc9dafc71eb10d30d460750279e
Jon Austin <jon.i.austin@gmail.com>
2020-03-21 15:40:21 -0700

    detail page

--- web/src/components/BlogPostCell/BlogPostCell.js
+++ web/src/components/BlogPostCell/BlogPostCell.js
@@ -1,7 +1,12 @@
+import { Link, routes } from '@redwoodjs/router'
+
 export const QUERY = gql`
-  query {
-    blogPost {
+  query($id: Int!) {
+    post(id: $id) {
       id
+      title
+      body
+      createdAt
     }
   }
 `
@@ -12,6 +17,14 @@ export const Empty = () => <div>Empty</div>
 
 export const Failure = ({ error }) => <div>Error: {error.message}</div>
 
-export const Success = ({ blogPost }) => {
-  return JSON.stringify(blogPost)
+export const Success = ({ post }) => {
+  return (
+    <article key={post.id}>
+      <header>
+        <h2>{post.title}</h2>
+      </header>
+      <p>{post.body}</p>
+      <div>Posted at: {post.createdAt}</div>
+    </article>
+  )
 }
