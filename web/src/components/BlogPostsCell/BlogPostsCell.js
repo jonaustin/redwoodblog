d0cb4e668dc190122860d447e5bf69c6046e703e
Jon Austin <jon.i.austin@gmail.com>
2020-03-21 15:25:49 -0700

    route with id

--- web/src/components/BlogPostsCell/BlogPostsCell.js
+++ web/src/components/BlogPostsCell/BlogPostsCell.js
@@ -1,3 +1,5 @@
+import { Link, routes } from '@redwoodjs/router'
+
 export const QUERY = gql`
   query {
     posts {
@@ -20,7 +22,9 @@ export const Success = ({ posts }) => {
     //console.log(JSON.stringify(post))
     <article key={post.id}>
       <header>
-        <h2>{post.title}</h2>
+        <h2>
+          <Link to={routes.blogPost({ id: post.id })}>{post.title}</Link>
+        </h2>
       </header>
       <p>{post.body}</p>
       <div>Posted at: {post.createdAt}</div>
